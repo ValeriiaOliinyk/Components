3edf989e1a85457f065f6e7ba7cecd351c61729e
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-06-14 22:17:54 +0300

    Updates

---
+++ precache-manifest.ca4bc0ece7c2e9ce99aef555c308da47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1758e90d66729d1f3f62dd3bfbc28238",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c919d66e4f5220644ffc",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "074378e917baac306478",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/css/main.28f75340.chunk.css"
+  },
+  {
+    "revision": "c919d66e4f5220644ffc",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/js/2.5a467cb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/js/2.5a467cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074378e917baac306478",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/js/main.8ef407bc.chunk.js"
+  },
+  {
+    "revision": "81b1ae27cfcf8b21ffa4",
+    "url": "/ValeriiaOliinyk/goit-react-hw-01-components/static/js/runtime-main.43bb7fbd.js"
+  }
+]);
